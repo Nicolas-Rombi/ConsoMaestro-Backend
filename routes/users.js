24b73c71c98dd391a12d740a3447304f1ec575bb
Marine <marinekervella@yahoo.fr>
2024-10-28 10:24:02 +0100

    Creation of connection access and comparison data

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const bcrypt = require('bcrypt');
 
+// User already exists in database
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['username', 'password'])) {
+    res.json({ result: false, error: 'élément manquant ou champ resté vide' });
+    return;
+  }
+  // The data entered by the user is compared to what's in the database
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'Utilisateur introuvable ou mot de passe incorrect' });
+    }
+  });
+});
 module.exports = router;
