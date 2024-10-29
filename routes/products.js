5fbf420e8b6ce83ad85c44cb2d8fc89f10269c41
Nicolas Rombi <nico461@hotmail.fr>
2024-10-29 10:51:09 +0100

    upc route

--- routes/products.js
+++ routes/products.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 const Product = require('../models/products');
 
-router.get('/', async (req, res) => {
+router.get('/:UPC', async (req, res) => {
+ const UPC = req.params.UPC;
  await fetch (`https://world.openfoodfacts.org/api/v0/product/${UPC}.json`)
  .then(response => response.json())
     .then(data => {
     const newProduct = new Product({
         name: data.product.product_name,
         image: data.product.image_url,
-        
+        upc: UPC,
     });
     newProduct.save().then(newDoc => {
         res.json({ result: true, message: 'Produit ajouté !' });
