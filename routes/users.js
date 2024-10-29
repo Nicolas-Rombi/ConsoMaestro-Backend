f99fc40cb5a3dcae8ccdd8066e8ffcc730194037
Nicolas Rombi <nico461@hotmail.fr>
2024-10-29 14:49:41 +0100

    add user id to useselector

--- routes/users.js
+++ routes/users.js
@@ -47,7 +47,7 @@ router.post('/signin', (req, res) => {
   // The data entered by the user is compared to what's in the database
   User.findOne({ username: req.body.username }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token });
+      res.json({ result: true, token: data.token, userId: data._id, });
     } else {
       res.json({ result: false, error: 'Utilisateur introuvable ou mot de passe incorrect' });
     }
