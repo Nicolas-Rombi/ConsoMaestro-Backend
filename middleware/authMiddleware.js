3d8de44b455d90930fd8e587f7355543f73d5b51
Your name <Your email>
2024-10-30 15:48:07 +0100

    modifs route et authmiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,27 +1,27 @@
-const jwt = require('jsonwebtoken');
+const User = require('../models/users');
 
-// Middleware d'authentification pour vérifier le token JWT
-const authMiddleware = (req, res, next) => {
+// Middleware d'authentification pour vérifier que le token dans la req matche avec la db
+const authMiddleware = async (req, res, next) => {
   // Récupère le token de l'en-tête Authorization (format "Bearer <token>")
   const token = req.headers['authorization']?.split(' ')[1];
+  console.log('req.headers:', req.headers)
+  console.log('token :', token);
 
   // Vérifie si un token est présent
   if (!token) {
     return res.status(401).json({ message: 'Accès refusé : aucun token fourni.' });
   }
-
+  
   try {
-    // Vérifie et décode le token avec la clé secrète
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Ajoute les informations de l'utilisateur décodées (ID, rôle, etc.) à la requête
-    req.user = decoded;
-
-    // Passe à la route suivante
-    next();
+    const user = await User.findOne({ token });
+    if (!user) {
+      return res.status(403).json({ message: 'Token invalide ou utilisateur non authentifié.' });
+    }
+    req.user = { id: user._id }; // Ajoute l'ID de l'utilisateur dans la req
+    next(); 
   } catch (error) {
-    // Gère les erreurs de validation du token
-    return res.status(403).json({ message: 'Token invalide.' });
+    console.error('Erreur d’authentification :', error);
+    return res.status(500).json({ message: 'Erreur du serveur lors de la vérification du token.' });
   }
 };
 
