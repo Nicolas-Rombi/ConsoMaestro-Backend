836c1cae292526873bfafbc370dca94fab62ea4f
Marine <marinekervella@yahoo.fr>
2024-11-01 12:36:54 +0100

    route Get products

--- routes/products.js
+++ routes/products.js
@@ -49,4 +49,20 @@ router.post('/:DLC', async (req, res) => {
     }
 });
 
+router.get('/products/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    
+    try {
+        const products = await Product.find({ user: userId }); // Rechercher les produits liés à l'utilisateur
+        if (products) {
+            res.json({ result: true, data: products });
+        } else {
+            res.json({ result: false, message: 'Aucun produit trouvé pour cet utilisateur.' });
+        }
+    } catch (error) {
+        console.error("Erreur lors de la récupération des produits :", error);
+        res.status(500).json({ result: false, message: 'Erreur serveur lors de la récupération des produits.' });
+    }
+});
+
 module.exports = router;
