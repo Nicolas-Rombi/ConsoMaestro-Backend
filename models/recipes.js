551e949ae10a2858447eb210b25e7f8011fe3299
Your name <Your email>
2024-11-07 09:50:00 +0100

    modif model recipes

--- models/recipes.js
+++ models/recipes.js
@@ -4,9 +4,9 @@ const recipesSchema = new mongoose.Schema({
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Référence à l'utilisateur
   products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }], // Liste de références de produits associés
   title: { type: String, required: true },
-  image: { type: String, required: true }, // Image en tant qu'URL (string)
+  image: { type: String, required: true }, 
   description: { type: String, required: true },
-  recipeId: { type: Number, required: true },
+  recipeId: { type: Number, required: true, unique: true },
 });
 
 const Recipe = mongoose.model('Recipe', recipesSchema);
